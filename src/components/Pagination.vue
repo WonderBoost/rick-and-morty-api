882562b18f6aba7ebab8923a7b7c83264b5a7eca
wonderboost <josegsv28@gmail.com>
2022-02-11 21:53:22 -0400

    fix sass deprecated

---
+++ src/components/Pagination.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="pages">
+      <div class="item" v-on:click="prev()">
+          Prev
+      </div>
+      <div class="item" v-on:click="next()">
+          Next
+      </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+    let counter = 1;
+export default {
+
+
+    setup() {
+        const store = useStore()
+
+        const filter = ((page) => {
+            store.dispatch('pageNumber', page)
+        })
+
+        return {
+            filter
+        }    
+    }
+}
+</script>
+
+<style lang="scss">
+.pages {
+        width: 400px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        border-radius: 10px;
+        overflow: hidden;
+    .item {
+        padding: 1rem 0.5rem;
+        background-color: var(--background-card);
+        text-align: center;
+        cursor: pointer;
+        &:hover {
+            color: var(--text-orange);
+        }
+    }
+}
+
+</style>
